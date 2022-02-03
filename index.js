97e7a6ae3484f12c196c8dce3e2496c498a45602
Zale Davis <zale.davis@gmx.com>
2022-02-02 20:50:34 -0800

    add sign in/sign out functionality

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const app = express();
 const User = require('./models/user');
 const bcrypt = require('bcrypt');
+const session = require('express-session');
 
 mongoose.connect('mongodb://localhost:27017/authDemo', { useUnifiedTopology: true })
     .then(() => {
@@ -18,6 +19,14 @@ app.set('views', 'views');
 
 //required to parse URL body
 app.use(express.urlencoded({ extended: true }));
+app.use(session({ secret: 'notagoodsecret' }));
+
+const requireLogin = (req, res, next) => {
+    if (!req.session.user_id) {
+        return res.redirect('/login');
+    }
+    next();
+}
 
 app.get('/', (req, res) => {
     res.send('THIS IS THE HOME PAGE');
@@ -35,6 +44,7 @@ app.post('/register', async (req, res) => {
         password: hash
     })
     await user.save();
+    req.session.user_id = user._id;
     res.redirect('/')
 })
 
@@ -44,17 +54,28 @@ app.get('/login', (req, res) => {
 
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
-    const user = await User.findOne({ username });
-    const validPassword = await bcrypt.compare(password, user.password);
-    if (validPassword) {
-        res.send("WELCOME!")
+    const foundUser = await User.findAndValidate(username, password)
+    if (foundUser) {
+        req.session.user_id = foundUser._id;
+        res.redirect('/secret');
     } else {
-        res.send("TRY AGAIN")
+        res.redirect('/login');
     }
 })
 
-app.get('/secret', (req, res) => {
-    res.send('THIS IS SECRET, CAN ONLY SEE IF LOGGED IN')
+app.post('/logout', (req, res) => {
+    req.session.user_id = null;
+    //eliminate all stored information:
+    //req.session.destroy();
+    res.redirect('/login');
+})
+
+app.get('/secret', requireLogin, (req, res) => {
+    res.render('secret');
+})
+
+app.get('/topsecret', requireLogin, (req, res) => {
+    res.send("SUPER TOP SECRET!!!!!")
 })
 
 app.listen(3000, () => {
